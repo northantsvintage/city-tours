fb6ae6396f6453f240310017b51dd368f5413610
northantsvintage <northantsvintage@gmail.com>
2020-08-27 13:19:15 +0100

    navbar component and scss

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.scss";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import NavBar from "./components/NavBar/NavBar";
+
+function App() {
+  return (
+    <React.Fragment>
+      <NavBar />
+    </React.Fragment>
+  );
+}
+
+export default App;
