a554825f4531413907538bfe3377918790f7b66d
northantsvintage <northantsvintage@gmail.com>
2020-08-27 14:10:48 +0100

    mapped through the tourData to list each tour component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.scss";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import NavBar from "./components/NavBar/NavBar";
+import TourList from "./components/TourList";
 
 function App() {
   return (
     <React.Fragment>
       <NavBar />
+      <TourList />
     </React.Fragment>
   );
 }
